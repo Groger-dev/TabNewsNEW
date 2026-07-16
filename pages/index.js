34e82994496d0b1e5a889915bd7f2b8bc15942a9
Roger, Gustavo <gustavormsousa01@gmail.com>
2026-07-16 03:03:27 +0000

    add `.prettierignore`

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 function Home() {
-  return;
-  <h1>
-    De Gustavo para Lívia, meu amor!Oiieeee amor! Passando pra dizer que te amo
-    muuuuuuuuuuuuuuuuuuuuito!!! Tamlooooooooo!!S2
-  </h1>;
+  return (
+    <h1>
+      De Gustavo para Lívia, meu amor!Oiieeee amor! Passando pra dizer que te
+      amo muuuuuuuuuuuuuuuuuuuuito!!! Tamlooooooooo!!S2
+    </h1>
+  );
 }
 
 export default Home;
