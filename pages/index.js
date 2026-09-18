1fd0e2a580df49eb7c5b74b731e7e4e2fa3e6904
Roger, Gustavo <gustavormsousa01@gmail.com>
2026-09-18 23:52:39 +0000

    Mudanca de texto

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 function Home() {
   return (
     <h1>
-      Oi meu amor! Feliz 0.9 ❤️‍🔥 Te amo muuuuuuuuuuito!!! Tamlooooooooo + 50x
+      Mais um mês ao seu lado, amo muuuuuito vc em cada detalhe ❤️‍🔥 Te amoooooooo
+      muito muito muito muito muito muito muitooooooo!!!! Feliz 11 meses amoror
+      ❤️
     </h1>
   );
 }
